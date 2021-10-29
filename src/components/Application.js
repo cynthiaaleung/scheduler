da0181fdd08ad6ff9e6afff2a557982b962ddb20
Cynthia Leung <81842196+cynthiaaleung@users.noreply.github.com>
2021-10-28 20:30:25 -0700

    use Axios to fetch data, retrieve days data from api, create selector function, refactor code

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import "components/Application.scss";
 import DayList from "./DayList";
 import Appointment from "./Appointment";
@@ -42,26 +43,29 @@ const appointments = [
   }
 ];
 
-const days = [
-  {
-    id: 1,
-    name: "Monday",
-    spots: 2,
-  },
-  {
-    id: 2,
-    name: "Tuesday",
-    spots: 5,
-  },
-  {
-    id: 3,
-    name: "Wednesday",
-    spots: 0,
-  },
-];
-
 export default function Application(props) {
-  const [day, setDay] = useState("Monday");
+  const [state, setState] = useState({
+    day: "Monday",
+    days: []
+  });
+
+  const setDay = day => setState({ ...state, day });
+
+  const setDays = (days) => setState(prev => ({ ...prev, days }));
+  
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:8001/api/days").then(response => {
+        setDays(response.data);
+      })
+      .catch(error => {
+        console.log(error.response.status);
+        console.log(error.response.headers);
+        console.log(error.response.data);
+      });
+
+  }, [])
 
   return (
     <main className="layout">
@@ -74,9 +78,9 @@ export default function Application(props) {
       <hr className="sidebar__separator sidebar--centered" />
       <nav className="sidebar__menu">
         <DayList
-          days={days}
-          value={day}
-          onChange={setDay}
+          days={state.days}
+          day={state.day}
+          setDay={setDay}
         />
       </nav>
       <img
---
+++ src/helpers/selectors.js
@@ -0,0 +1,21 @@
+export function getAppointmentsForDay(state, day) {
+
+  const filteredAppointments = [];
+
+  // find day object in days using day
+  const filteredDay = state.days.filter(dayObj => dayObj.name === day);
+
+  if (filteredDay.length === 0) {
+    return [];
+  }
+
+  // get the array of appointments id
+  const apptIds = filteredDay[0].appointments;
+
+  // get all the appointment objects
+  for (let apptId of apptIds) {
+    filteredAppointments.push(state.appointments[apptId]);
+  }
+
+  return filteredAppointments;
+}
