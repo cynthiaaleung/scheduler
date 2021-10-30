2444d866b34a75262e784233a447dc123f65a5bc
Cynthia Leung <81842196+cynthiaaleung@users.noreply.github.com>
2021-10-29 22:15:00 -0700

    install react-test-renderer, build custom hook to manage visual mode of any component

---
+++ src/hooks/useVisualMode.js
@@ -0,0 +1,25 @@
+import { useState } from "react";
+
+export default function useVisualMode(initial) {
+  const [mode, setMode] = useState(initial);
+  const [history, setHistory] = useState([initial]);
+
+  function transition(newMode, replace = false) {
+    setMode(newMode)
+
+    if (!replace) {
+      const newHistory = [...history];
+      newHistory.push(newMode);
+      setHistory(newHistory);
+    }
+  }
+
+  function back() {
+    if (history.length > 1) {
+      history.pop();
+      setMode(history[history.length - 1]);
+    }
+  }
+
+  return { mode, transition, back };
+}
